6443d27e283f0313a1ede3ea8dc509ad43a0ae02
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-01 18:00:53 +0900

    인풋 입력시 검색 결과 출력 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { search } from "./api"
 
 const App = () => {
 
-  useEffect(() => {
-    searchHandler();
-  })
+  const [input, setInput] = useState(''); // input을 통한 검색어 
+  const [results, setResults] = useState([]); // 검색 결과
 
-  const searchHandler = async () => {
+  // useEffect(() => {
+  //   searchHandler('default');
+  // })
+
+  // api 검색 기능 함수
+  const searchHandler = async (query) => {
     // 검색 파라미터 설정
     const params = {
-      query: "호로록",
+      query: query,
       sort: "accuracy",
       page: 1,
       size: 10
     }
 
     const { data } = await search(params); // 검색 api 호출
-    console.log(data); // 결과 호출
+    // console.log(data); // 결과 호출
+    setResults(data.documents.map((item) => item.contents)); // 결과값 state에 저장
+  }
+
+  // 인풋 입력시 실행할 함수
+  const handleInputUpdate = (e) => {
+    setInput(e.target.value);
+    input !== "" && searchHandler(input); // 검색 함수 실행
   }
   
 
@@ -28,8 +39,15 @@ const App = () => {
         type="search" 
         placeholder='검색어를 입력하세요'
         name="query"
+        onChange={handleInputUpdate}
+        value={input}
         />
       </div>
+      <ul>
+        {results.map((result, idx) => {
+          return <li key={idx}>{result}</li>
+        })}
+      </ul>
     </>
   )
 }
