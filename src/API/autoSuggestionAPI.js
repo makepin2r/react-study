7be0f2bcb2f59f8d188e8509063fc0aaf459b0a9
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-01 19:23:02 +0900

    검색어 자동완성 기능 구현

---
+++ src/API/autoSuggestionAPI.js
@@ -0,0 +1,10 @@
+// 카카오 검색 API 세팅
+import axios from "axios";
+
+const amazon = axios.create();
+
+// 검색 api
+export const autoSuggest = (inputValue) => {
+    return amazon.get(`https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=71&prefix=${inputValue}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`);
+};
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import { search } from "./API/searchAPI"
+import { autoSuggest } from "./API/autoSuggestionAPI"
 
 const App = () => {
 
   const [input, setInput] = useState(''); // input을 통한 검색어 
   const [results, setResults] = useState([]); // 검색 결과
+  const [keywords, setKeywords] = useState([]); // 검색어 자동완성
 
   // api 검색 기능 함수
   const searchHandler = async (query) => {
@@ -18,18 +20,26 @@ const App = () => {
 
     const { data } = await search(params); // 검색 api 호출
     setResults(data.documents.map((item) => {
-      console.log(item)
       return { title: item.title, content: item.contents, url: item.url }
     })); // 결과값 state에 저장
-    console.log(results)
   }
+
+  // 검색어 자동완성 함수
+  const autoSuggestionHandler = async (query) => {
+    const { data } = await autoSuggest(query); // 자동완성 호출
+    data.suggestions.length !== 0 && setKeywords(data.suggestions.map((item) => {
+      return { value: item.value }
+    })); // 결과값 state에 저장
+  }
+
+
 
   // 인풋 입력시 실행할 함수
   const handleInputUpdate = (e) => {
     setInput(e.target.value);
     if(input !== ""){
       searchHandler(input); // 검색 함수 실행
-      // 검색어 자동완성 함수 실행
+      autoSuggestionHandler(input);// 검색어 자동완성 함수 실행
     }
   }
   
@@ -44,6 +54,13 @@ const App = () => {
         onChange={handleInputUpdate}
         value={input}
         />
+        <div>
+          <ul>
+            {keywords.map((keyword, idx) => {
+              return <li key={idx}>{keyword.value}</li>
+            })}
+          </ul>
+        </div>
       </div>
       <ul>
         {results.map((result, idx) => {
