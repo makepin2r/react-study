e4a61bfa0a0da1ff6da1902a8f80df1326a37205
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-08 15:53:31 +0900

    프로젝트 기본 세팅

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import GlobalStyle from './assets/GlobalStyle'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle/>
+      <div></div>
+    </>
+  )
+}
+
+export default App
