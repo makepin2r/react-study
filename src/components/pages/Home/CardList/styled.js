16653da2fa7f8f60754f13dcbb346a98268f1a4d
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-09 12:17:53 +0900

    style: Card, CardList 스타일링

---
+++ src/components/pages/Home/CardList/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StCardList = styled.ul`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
